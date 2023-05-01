58938fdf2168488886bb55d903bc294b51eaf19b
sharad <sharad@mineo333.dev>
2023-05-01 02:20:16 -0400

    I'm an idiot

--- qemu-script-hyp.sh
+++ qemu-script-hyp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pkill -9 qemu #kill all existing version of qemu
+
 ./qemu-system-aarch64 -M virt -cpu cortex-a57 -nographic -kernel bin/hyp.mbn -machine virtualization=on -machine gic-version=3 -monitor telnet:127.0.0.1:1235,server,nowait -s -S -smp 1 -m 4G -d int -device loader,file="bin/devcfg.mbn"
 
 
